cc2d454aecff343ebb5c3bba48b9954a96a22489
aerealis <73922794+feraldeveloper@users.noreply.github.com>
2025-03-28 16:39:52 -0700

    Update client.js

--- client.js
+++ client.js
@@ -1,19 +1,13 @@
-// Replace the firebaseConfig object below with your Firebase project details
-  const firebaseConfig = {
+const firebaseConfig = {
+  apiKey: "AIzaSyB7iEdtr_us1-mJQ1iFhWRCFdy5G5cjals",
+  authDomain: "at-c-b27b4.firebaseapp.com",
+  databaseURL: "https://at-c-b27b4-default-rtdb.firebaseio.com",  // <-- Your database URL here
+  projectId: "at-c-b27b4",
+  storageBucket: "at-c-b27b4.firebasestorage.app",
+  messagingSenderId: "501080306491",
+  appId: "1:501080306491:web:928a6bcc11b1801787bd1e"
+};
 
-    apiKey: "AIzaSyB7iEdtr_us1-mJQ1iFhWRCFdy5G5cjals",
-
-    authDomain: "at-c-b27b4.firebaseapp.com",
-
-    projectId: "at-c-b27b4",
-    databaseURL: "https://at-c-b27b4-default-rtdb.firebaseio.com",
-    storageBucket: "at-c-b27b4.firebasestorage.app",
-
-    messagingSenderId: "501080306491",
-
-    appId: "1:501080306491:web:928a6bcc11b1801787bd1e"
-
-  };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
